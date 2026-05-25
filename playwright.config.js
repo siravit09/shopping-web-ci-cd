f110bd9df396587bfcba8d344553516cd61c1c03
Siravit Suntiwong <siravit.s@wisdomvast.com>
2026-05-25 16:49:44 +0700

    start app before playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+//import { defineConfig, devices } from '@playwright/test';
+const { defineConfig, devices } = require('@playwright/test');
 
 /**
  * Read environment variables from file.
@@ -12,7 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-export default defineConfig({
+//export default defineConfig({
+module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -26,11 +28,18 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    baseURL: 'http://127.0.0.1:3000',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 
   /* Configure projects for major browsers */
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Login Pass', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/');
+  await page.goto('/');
   await expect(page).toHaveTitle(/Simple Shop/);
 
   await page.locator('input[name="username"]').fill('demo');
